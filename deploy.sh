9d33ff0b751841456bf43fec37b21268403d892b
Paul Golder <paul.a.golder@lerot.org>
2019-02-13 11:38:38 +0000

    update

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,6 @@ tar cf "$FILE" src
 tar --append --file="$FILE"  "translations"
 tar --append --file="$FILE"  "web/css"
 tar --append --file="$FILE"  "web/js"
-tar --append --file="$FILE"  "web/kml"
 tar --append --file="$FILE"  "app/config/config.yml"
 tar --append --file="$FILE"  "app/config/parameters_server.yml"
 tar --append --file="$FILE"  "app/config/routing.yml"
@@ -18,8 +17,5 @@ tar --append --file="$FILE"  "app/config/services.yml"
 tar --append --file="$FILE"  "app/Resources"
 tar --append --file="$FILE"  "app/AppKernel_server.php"
 
-#tar  --transform 's,^AppKernel_server.php,AppKernal_x.php,'   cf  --file= "$FILE"  cf
-#sed 's/_server/_freddy/g'  <$FILE  >newfile.tar
 
-#gzip newfile.tar
 gzip "$FILE"  
