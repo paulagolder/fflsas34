d5a749a5d181947ac3915b72f1a23e2f4b65f768
Paul Golder <paul.a.golder@lerot.org>
2019-02-18 09:49:09 +0000

    update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new FOS\CKEditorBundle\FOSCKEditorBundle(),
             new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
-            new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             
--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,10 @@ tar cf "$FILE" src
 tar --append --file="$FILE"  "translations"
 tar --append --file="$FILE"  "web/css"
 tar --append --file="$FILE"  "web/js"
-tar --append --file="$FILE"  "app/config/config.yml"
-tar --append --file="$FILE"  "app/config/parameters_server.yml"
 tar --append --file="$FILE"  "app/config/routing.yml"
 tar --append --file="$FILE"  "app/config/security.yml"
 tar --append --file="$FILE"  "app/config/services.yml"
 tar --append --file="$FILE"  "app/Resources"
-tar --append --file="$FILE"  "app/AppKernel_server.php"
 
 
 gzip "$FILE"  
